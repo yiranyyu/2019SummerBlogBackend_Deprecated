259ee4990d47a38aedfbbb7d1fe69905d0c4782e
yiranyyu <2606375857@qq.com>
2019-08-25 14:25:30 +0800

    Fix typo

--- src/main/java/buaasoft/blog/mapping/LoginAndSignUpPage.java
+++ src/main/java/buaasoft/blog/mapping/LoginAndSignUpPage.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @Controller
-@RequestMapping(value = "/logins")
+@RequestMapping(value = "/login")
 public class LoginAndSignUpPage {
     @Autowired
     private UserRepository userRepository;
